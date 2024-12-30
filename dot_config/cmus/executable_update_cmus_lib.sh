9ed10702fa6958390740b01e8882399541165715
Marek Kloza <kloza.marek@gmail.com>
2024-12-30 16:23:02 +0100

    min solaar removed

--- dot_config/cmus/executable_update_cmus_lib.sh
+++ dot_config/cmus/executable_update_cmus_lib.sh
@@ -1,7 +1,4 @@
 #!/bin/zsh
 cmus-remote -C clear
-cmus-remote -C "add /run/media/ssserpent/gen_dta/Music"
-cmus-remote -C "add /run/media/ssserpent/intenso/Music/MP3_JAZZ"
-cmus-remote -C "add /run/media/ssserpent/intenso/Music/MP3_OTHER"
-cmus-remote -C "add /run/media/ssserpent/HDD500_2"
+cmus-remote -C "add /home/ssserpent/Music"
 cmus-remote -C "update-cache -f"
