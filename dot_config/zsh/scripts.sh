a7ed960dee8daa29cf19e30e46fda8be87b95b5c
Marek Kloza <kloza.marek@gmail.com>
2024-12-30 17:08:19 +0100

    sgpt shell integration added

--- dot_config/zsh/scripts.sh
+++ dot_config/zsh/scripts.sh
@@ -578,3 +578,16 @@ tsm-show() { transmission-show "$1" ;}                          # show .torrent 
 # DESC: ncurses frontend to transmission-daemon
 tsm-ncurse() { transmission-remote-cli ;}
 #}}}
+# -- Shell-GPT integration ZSH v0.2 --{{{
+_sgpt_zsh() {
+if [[ -n "$BUFFER" ]]; then
+    _sgpt_prev_cmd=$BUFFER
+    BUFFER+="⌛"
+    zle -I && zle redisplay
+    BUFFER=$(sgpt --shell <<< "$_sgpt_prev_cmd" --no-interaction)
+    zle end-of-line
+fi
+}
+zle -N _sgpt_zsh
+bindkey "^l" _sgpt_zsh
+#}}}
